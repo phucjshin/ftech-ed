dc9fa4263a16134bf41203053f48e5510d59486b
Dung Nguyen <dungntnew@gmail.com>
2018-04-22 16:21:10 +0700

    use cdn for vendor - now limit data size < 1MB

---
+++ data/dzung-team-info.js
@@ -0,0 +1,11 @@
+/* 
+sample data:
+fields: id, position, fullname, phone, hobby
+*/
+
+const data = [
+	{id: 1, position: 'CTO', fullname: 'Nguyen Tri Dzung', phone: 'xxxxxxxxx', hobby: 'Code' },
+	{id: 2, position: 'CTO', fullname: 'Hoang Huy Minh', phone: 'yyyyyyyy', hobby: 'Code' },
+	{id: 3, position: 'CEO', fullname: 'Vu Van Tu', phone: 'zzzzzzzz', hobby: 'Code' },
+	{id: 4, position: 'COO', fullname: 'Le Viet Canh', phone: 'kkkkkkkkk', hobby: 'Code' },
+]

e553b4e8d29a13fe6987fe4097ca43eaf5f1c9fc
Trần Mạnh Thắng <thangtm@vatgia.com>
2018-04-24 20:42:15 +0700

    thang sua file

--- data/dat-team-info.js
+++ data/dat-team-info.js
@@ -4,5 +4,7 @@ fields: id, position, fullname, phone, hobby
 */
 
 const data = [
-	{id: 1, position: 'DEV', fullname: 'Tran Thanh Dat', phone: '0979794298', hobby: 'Code' }
+	{id: 1, position: 'DEV', fullname: 'Tran Thanh Dat', phone: '0979794298', hobby: 'Code' },
+	{id: 2, position: 'DEV', fullname: 'Tran Manh Thang', phone: '01696366422', hobby: 'Code' },
+
 ]

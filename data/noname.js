d3f155d3acdfcc7fad09af3a306fb38a3eb5c38b
@tt-01-05-18-dung <vuvandung_t59@hus.edu.vn>
2018-04-24 20:51:51 +0700

    edit noname1 lan 1

---
+++ data/noname.js
@@ -0,0 +1,9 @@
+/* 
+sample data:
+fields: id, position, fullname, phone, hobby
+*/
+
+const data = [
+	{id: 1, position: 'teamLeader', fullname: 'Vu Van Dung', phone: 'xxxxxxxxx', hobby: 'Code' },
+
+]

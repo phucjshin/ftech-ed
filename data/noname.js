f03b4f1ec3cef6608afb1760981e8df75b8995a4
@tt-01-05-18-dung <vuvandung_t59@hus.edu.vn>
2018-04-24 21:02:39 +0700

    edit noname1 final

--- data/noname.js
+++ data/noname.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-const data = [
-	{id: 1, position: 'Leader', fullname: 'Pham Anh Duc ', phone: '0962020536', hobby: 'Code' },
-	
-=======
 /* 
 sample data:
 fields: id, position, fullname, phone, hobby

3f580d1848a752bb00241f5924ff525a8cb4e711
hoannc1 <38690707+hoannc1@users.noreply.github.com>
2018-04-24 20:45:06 +0700

    nguyen cong hoan add info

--- data/dat-team-info.js
+++ data/dat-team-info.js
@@ -5,4 +5,5 @@ fields: id, position, fullname, phone, hobby
 
 const data = [
 	{id: 1, position: 'DEV', fullname: 'Tran Thanh Dat', phone: '0979794298', hobby: 'Code' }
+	{id: 2, position: 'DEV', fullname: 'Nguyen Cong Hoan', phone: '01689406151', hobby: 'Code' }
 ]

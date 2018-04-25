833e9770235538150b870d1ffd7dcc2c2b92b9ef
phucjshin <phucnv.94ptit@gmail.com>
2018-04-25 20:56:37 +0700

    add khang and trong's informaton

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -1,5 +1,7 @@
 
 const data = [
-	{id: 1, position: 'dev', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
+	{id: 1, position: 'dev', fullname: 'Phuc', phone: 'xxxxxxxx', hobby: 'Code' },
+	{id: 2, position: 'dev', fullname: 'Khang', phone: 'xxxxxxx', hobby: 'Code' },
+	{id: 3, position: 'dev', fullname: 'Trong', phone: 'xxxxxxx', hobby: 'Code' },
 
 ]

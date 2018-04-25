8aa76a93225678e2c9936f5359356633f04590d9
Nguyen Van Phuc <you@example.com>
2018-04-25 19:12:27 +0700

    update information phuc-team

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -1,5 +1,5 @@
 
 const data = [
-	{id: 1, position: 'GiamDoc', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
+	{id: 1, position: 'dev', fullname: 'NguyenVanPhuc', phone: '124765432', hobby: 'Code' },
 
 ]

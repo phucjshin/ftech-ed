f85ccbdb8ecca2d736da86989c245979ac25506c
phucjshin <phucnv.94ptit@gmail.com>
2018-04-26 16:10:27 +0700

    phuc edit possion

--- data/phuc-team-info.js
+++ data/phuc-team-info.js
@@ -1,6 +1,6 @@
 
 const data = [
-	{id: 1, position: 'security', fullname: 'Phuc', phone: 'xxxxxxxx', hobby: 'Code' },
+	{id: 1, position: 'sss', fullname: 'Phuc', phone: 'xxxxxxxx', hobby: 'Code' },
 	{id: 2, position: 'dev', fullname: 'Khang', phone: 'xxxxxxx', hobby: 'Code' },
 
 ]
